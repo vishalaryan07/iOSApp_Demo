2536161db68d49efeaa2ab1b1a143365665bb83a
Vishal Ranjan <vishal.ranjan@tavant.com>
2017-06-15 11:41:52 +0530

    Initial commit

---
+++ iOSApp Demo/iOSApp Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOSApp Demo
+//
+//  Created by Vishal Ranjan on 14/06/17.
+//  Copyright © 2017 Vishal Ranjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .orange
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
